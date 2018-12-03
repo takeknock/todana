4f209004653cf8aa4ad693da59af3f24d06a07c5
mmatthew <takeknock@gmail.com>
2018-12-04 01:37:22 +0900

    feat: add sample api

---
+++ Todana/Controllers/ErrorController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Todana.Controllers
+{
+    public class ErrorController : ApiController
+    {
+        public string Get()
+        {
+            return "GET Success!!";
+        }
+    }
+}
